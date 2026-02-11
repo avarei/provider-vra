fa3870848bdd80d5e895a96e182e7eac329187b5
Tim Geimer <32556895+Avarei@users.noreply.github.com>
2026-02-11 11:29:45 +0100

    keep cluster scoped resources backwards compatible

--- config/provider.go
+++ config/provider.go
@@ -63,7 +63,7 @@ var providerMetadata string
 // GetProvider returns provider configuration
 func GetProvider() *ujconfig.Provider {
 	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
-		ujconfig.WithRootGroup("vra.crossplane.io"),
+		ujconfig.WithRootGroup("crossplane.io"),
 		ujconfig.WithIncludeList(ExternalNameConfigured()),
 		ujconfig.WithFeaturesPackage("internal/features"),
 		ujconfig.WithDefaultResourceOptions(
